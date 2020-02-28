352b11531a15f29f8f9a31e6c19239e7d7f55266
Joshua Taguicana <joshtaguicana@gmail.com>
2020-02-27 21:15:10 -0500

    updated cherry blossom code

--- cherry-blossoms/main.js
+++ cherry-blossoms/main.js
@@ -1,26 +1,21 @@
 
 function fallingPetals() {
   let petal = document.querySelectorAll('.petal');
-  
-  if (!petal[0].animate) {
-    return false;
-  }
 
-  for (let i = 0, length = petal.length; i < length; ++i) {
-    let block = petal[i];
-    block.innerHTML = '<div class="rotate"><img src="petal.svg" class="skewing"/></div>';
+  petal.forEach(p => {
+    p.innerHTML = '<div class="rotate"><img src="petal.svg" class="skewing"/></div>';
     let scale = Math.random() * .8 + .2;
-    let animationAdd = block.animate([
-      { transform: `translate3d(${i/length*100}vw,0,0) scale(${scale})`, opacity: scale },
-      { transform: `translate3d(${i/length*100 + 15}vw,100vh,0) scale(${scale})`, opacity: 1 }
+    let i = Math.floor(Math.random() * Math.floor(petal.length))
+    
+    p.animate([
+      { transform: `translate3d(${i/petal.length*100}vw,0,0) scale(${scale})`, opacity: scale },
+      { transform: `translate3d(${i/petal.length*100 + 25}vw,100vh,0) scale(${scale})`, opacity: 1 }
     ], {
-      duration: Math.random() * 20000 + 4000,
+      duration: Math.random() * 25000 + 3000,
       iterations: Infinity,
       delay: -(Math.random() * 5000)
     });
-    
-    console.log(scale, animationAdd, (i/length*100))
-  }
+  })
 }
 
 fallingPetals();
